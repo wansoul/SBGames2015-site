5b9390fd02e85029df323d7aa3293c09eb654f1c
wan <wansoul@gmail.com>
2015-05-15 14:31:48 -0300

    Alteracoes na pagina de organização add de algumas imos de membros
    Alteracoes na pagina de organização add de algumas imos de membros

---
+++ app/organizacao/organizacaoController.js
@@ -0,0 +1,13 @@
+angular.module('sbgamesApp')
+	.controller('OrganizacaoController', 
+		function organizacaoController($http){
+			'use strict';
+			var org = this;
+
+			$http.get("app/organizacao/membros.json").success( 
+				function(response){
+					org.membros = response.membros;
+					console.log("response: " + response.membros);
+				}
+			);
+		});
